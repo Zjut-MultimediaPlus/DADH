29efcf8dc6cd69e4d1c9f879b84254cce87ccf43
zjut-zc <67258878+zjut-zc@users.noreply.github.com>
2020-11-27 20:25:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,26 +11,26 @@ class Default(object):
     vis_env = 'main'  # visdom env
     vis_port = 8097  # visdom port
 
-    # # for flickr25k dataset
-    # dataset = 'flickr25k'
-    # data_path = './data/FLICKR-25K.mat'
-    # db_size = 18015
-    # num_label = 24
-    # query_size = 2000
-    # text_dim = 1386
-    # training_size = 10000
-
-    # # # for nus-wide dataset
-    dataset = 'nus-wide'
-    # # data_path = './data/NUS-WIDE-TC10.mat'
-    data_path = './data/NUS-WIDE-TC21.mat'
-    db_size = 193734
-    # # db_size = 184457
-    num_label = 21
-    # # num_label = 10
-    query_size = 2100
-    text_dim = 1000
+    # for flickr25k dataset
+    dataset = 'flickr25k'
+    data_path = './data/FLICKR-25K.mat'
+    db_size = 18015
+    num_label = 24
+    query_size = 2000
+    text_dim = 1386
     training_size = 10000
+
+#     # # # for nus-wide dataset
+#     dataset = 'nus-wide'
+#     # # data_path = './data/NUS-WIDE-TC10.mat'
+#     data_path = './data/NUS-WIDE-TC21.mat'
+#     db_size = 193734
+#     # # db_size = 184457
+#     num_label = 21
+#     # # num_label = 10
+#     query_size = 2100
+#     text_dim = 1000
+#     training_size = 10000
 
     batch_size = 128
     image_dim = 4096
@@ -41,13 +41,13 @@ class Default(object):
     max_epoch = 300
 
     output_dim = 64  # hash code length
-    lr = 0.0006  # initial learning rate
+    lr = 0.0001  # initial learning rate
 
     device = 'cuda:0'
 
     # hyper-parameters
     alpha = 1
-    gamma = 0.00001
+    gamma = 0.001
     eta = 1
     mu = 0.5
     delta = 1
